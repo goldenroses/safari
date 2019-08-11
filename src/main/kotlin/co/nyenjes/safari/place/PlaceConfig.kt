1bce50855c8c82dabc0284f56102a47b08967066
Makena <makenaroes@gmail.com>
2019-08-11 18:44:22 +0300

    add update image url

--- src/main/kotlin/co/nyenjes/safari/place/PlaceConfig.kt
+++ src/main/kotlin/co/nyenjes/safari/place/PlaceConfig.kt
@@ -1,16 +1,15 @@
 package co.nyenjes.safari.place
 
-import com.zaxxer.hikari.HikariConfig
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.jdbc.DataSourceBuilder
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.orm.jpa.JpaTransactionManager
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.transaction.PlatformTransactionManager
 import org.springframework.transaction.annotation.EnableTransactionManagement
 import javax.persistence.EntityManagerFactory
